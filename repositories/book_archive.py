b414f263ad3a9ea5dea930d46a3b0a01fb3ec612
Lucas Araujo <93217820+Lurajo@users.noreply.github.com>
2022-11-19 10:42:35 -0300

    Atualização de BookArchive.py

--- repositories/book_archive.py
+++ repositories/book_archive.py
@@ -3,17 +3,19 @@ from repositories.book_repository import BookRepository
 
 
 class BookArchive:
-    file_book = list(open("books.csv", "r", encoding="utf-8"))
-    book_repository = BookRepository()
 
-    def format_str_price_to_float(price: str) -> float:
+    def __init__(self, book_repository: BookRepository) -> None:
+        self.file_book = list(open("books.csv", "r", encoding="utf-8"))
+        self.book_repository = book_repository
+
+    def format_str_price_to_float(self, price: str) -> float:
         try:
             return float(price.replace("R$ ", "").replace(",", "."))
         except:
             return 0
 
-    for book in file_book[1:]:
-        list_book = book.split(";")
-        book = Book(int(list_book[0]), list_book[1], list_book[2], list_book[3],
-                    list_book[4], format_str_price_to_float(list_book[5]))
-        book_repository.list_books.append(book)
+    def insert_book_in_repository(self):
+        for book in self.file_book[1:]:
+            list_book = book.split(";")
+            book = Book(int(list_book[0]), list_book[1], list_book[2], list_book[3], list_book[4], self.format_str_price_to_float(list_book[5]))
+            self.book_repository.list_books.append(book)
