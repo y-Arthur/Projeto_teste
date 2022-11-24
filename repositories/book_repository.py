7c290fabc82944ffc2f04a9184cd85ab6494a9fe
Arthur Cardoso <91923216+y-Arthur@users.noreply.github.com>
2022-11-23 21:02:36 -0300

    Método da impressão dos livros criados

--- repositories/book_repository.py
+++ repositories/book_repository.py
@@ -20,3 +20,14 @@ class BookRepository:
                 return book
 
         return Book(-1, "Livro não encontrado!", "", "", "", 0)
+    
+    def __str__(self) -> str:
+        formatText = "{0:<10} {1:<20} {1:<20} {1:<20} {1:<20} {1:<20}\n"
+        books = ("\n***** Relatório de livros cadastrados *****\n")
+        books += formatText.format("Id", "Título", "ISBN",
+                                  "Autor", "Assunto", "Valor", "Estoque")
+
+        for book in self.list_books:
+            books += formatText.format(book.id, book.name, book.isbn,
+                                      book.author, book.category, book.price, book.stock)
+        return books
